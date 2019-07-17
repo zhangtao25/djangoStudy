e123ad02c765b4a23176420ee84a48c6d22f4cd3
Allen Zhang <tzhangm@ctrip.com>
2019-07-17 19:58:20 +0800

    .

--- djangoStudy/settings.py
+++ djangoStudy/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mysite'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,7 @@ WSGI_APPLICATION = 'djangoStudy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': None
     }
 }
 
---
+++ mysite/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+
+# Create your models here.
+# import mongoengine
+# class StudentModel(mongoengine.document):
+#     name = mongoengine.StringField(max_length=16)
+#     age = mongoengine.ImageField(default=0)
